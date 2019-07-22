a1ff2125f7dc2e69e822a54e037ab343827547a1
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-07-22 19:36:01 +0900

    _app.js

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -1,19 +1,7 @@
-import Head from "next/head";
-import AppLayout from "../components/AppLayout";
-
 const Profile = () => {
   return (
     <>
-      <Head>
-        <title>NodeBird</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css"
-        />
-      </Head>
-      <AppLayout>
-        <div>My Profile</div>
-      </AppLayout>
+      <div>My Profile</div>
     </>
   );
 };
--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -1,47 +1,17 @@
 import { Button, Checkbox, Form, Input } from "antd";
-import Head from "next/head";
-import { useState } from "react";
-import AppLayout from "../components/AppLayout";
+import { useCallback, useState } from "react";
 
 const Signup = () => {
   const useInput = (initialValue = null) => {
     const [value, setter] = useState(initialValue);
-    const handler = event => {
+    const handler = useCallback(event => {
       const {
         target: { value: inputValue }
       } = event;
       setter(inputValue);
-    };
+    }, []);
 
     return [value, handler];
-  };
-
-  const onChangePasswordConfirm = event => {
-    const {
-      target: { value: inputValue }
-    } = event;
-
-    setPasswordError(inputValue !== password);
-    setPasswordConfirm(inputValue);
-  };
-
-  const onChangeTerm = event => {
-    const {
-      target: { checked }
-    } = event;
-    setTermError(false);
-    setTerm(checked);
-  };
-
-  const onSubmit = event => {
-    event.preventDefault();
-    if (password !== passwordConfirm) {
-      return setPasswordError(true);
-    }
-    if (!term) {
-      return setTermError(true);
-    }
-    console.log({ id, nickName, password, passwordConfirm, term });
   };
 
   const [id, onChangeId] = useInput("");
@@ -54,73 +24,97 @@ const Signup = () => {
   const [termError, setTermError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
 
+  const onChangePasswordConfirm = useCallback(
+    event => {
+      const {
+        target: { value: inputValue }
+      } = event;
+
+      setPasswordError(inputValue !== password);
+      setPasswordConfirm(inputValue);
+    },
+    [password]
+  );
+
+  const onChangeTerm = useCallback(event => {
+    const {
+      target: { checked }
+    } = event;
+    setTermError(false);
+    setTerm(checked);
+  }, []);
+
+  const onSubmit = useCallback(
+    event => {
+      event.preventDefault();
+      if (password !== passwordConfirm) {
+        return setPasswordError(true);
+      }
+      if (!term) {
+        return setTermError(true);
+      }
+    },
+    [password, passwordConfirm, term]
+  );
+
   return (
     <>
-      <Head>
-        <title>NodeBird</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css"
-        />
-      </Head>
-      <AppLayout>
-        <Form onSubmit={onSubmit} style={{ padding: 20 }}>
-          <div>
-            <label htmlFor="user-id">ID</label>
-            <Input
-              type="text"
-              value={id}
-              name="user-id"
-              onChange={onChangeId}
-              required={true}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-nickname">Nick Name</label>
-            <Input
-              type="text"
-              value={nickName}
-              name="user-nickname"
-              onChange={onChangeNickName}
-              required={true}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password">Password</label>
-            <Input
-              type="password"
-              value={password}
-              name="user-password"
-              onChange={onChangePassword}
-              required={true}
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password-confirm">Password Confirm</label>
-            <Input
-              type="password"
-              value={passwordConfirm}
-              name="user-password-confirm"
-              onChange={onChangePasswordConfirm}
-              required={true}
-            />
-            {passwordError && (
-              <div style={{ color: "red" }}>Password Does Not Match</div>
-            )}
-          </div>
-          <div>
-            <Checkbox name="user-term" value={term} onChange={onChangeTerm}>
-              Agree
-            </Checkbox>
-            {termError && <div style={{ color: "red" }}>You Need Agree.</div>}
-          </div>
-          <div style={{ marginTop: 20 }}>
-            <Button type="primary" htmlType="submit">
-              Sign up
-            </Button>
-          </div>
-        </Form>
-      </AppLayout>
+      <Form onSubmit={onSubmit} style={{ padding: 20 }}>
+        <div>
+          <label htmlFor="user-id">ID</label>
+          <Input
+            type="text"
+            value={id}
+            name="user-id"
+            onChange={onChangeId}
+            required={true}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-nickname">Nick Name</label>
+          <Input
+            type="text"
+            value={nickName}
+            name="user-nickname"
+            onChange={onChangeNickName}
+            required={true}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password">Password</label>
+          <Input
+            type="password"
+            value={password}
+            name="user-password"
+            onChange={onChangePassword}
+            required={true}
+          />
+        </div>
+        <div>
+          <label htmlFor="user-password-confirm">Password Confirm</label>
+          <Input
+            type="password"
+            value={passwordConfirm}
+            name="user-password-confirm"
+            onChange={onChangePasswordConfirm}
+            required={true}
+          />
+          {passwordError && (
+            <div style={{ color: "red" }}>Password Does Not Match</div>
+          )}
+        </div>
+        <div>
+          <Checkbox name="user-term" value={term} onChange={onChangeTerm}>
+            Agree
+          </Checkbox>
+          {termError && <div style={{ color: "red" }}>You Need Agree.</div>}
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <Button type="primary" htmlType="submit">
+            Sign up
+          </Button>
+        </div>
+      </Form>
     </>
   );
 };
