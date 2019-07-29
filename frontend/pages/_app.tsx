18f6834de83a8344bb364677dfdda84939aa4b19
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2019-07-29 16:01:49 +0800

    Connect Saga Middleware to Redux

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,8 +4,10 @@ import PropTypes from "prop-types";
 import { Provider } from "react-redux";
 import { applyMiddleware, compose, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
 import AppLayout from "../components/AppLayout";
 import reducer from "../reducers";
+import rootSaga from "../saga";
 
 const NodeBird = ({ Component, store }) => {
   return (
@@ -30,9 +32,11 @@ NodeBird.propTypes = {
 };
 
 export default withRedux((initialState, options) => {
-  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
   const composeEnhancers = composeWithDevTools({});
   const enhancer = composeEnhancers(applyMiddleware(...middlewares));
   const store = createStore(reducer, initialState, enhancer);
+  sagaMiddleware.run(rootSaga);
   return store;
 })(NodeBird);
